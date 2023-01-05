2dede63fb3356c0dad98e9d67b9eb20cc2e7ae84
Jackson <z5397791@ad.unsw.edu.au>
2023-01-06 08:56:31 +1100

    Set up homepage with sidebar component

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Twitter</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
+
+        {/* Modal */}
+      </main>
+    </div>
+  );
+}
