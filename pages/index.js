0c6fd709bbbd4f4569cec2881fa03e34d35b2059
Jackson <z5397791@ad.unsw.edu.au>
2023-01-06 16:27:23 +1100

    Add feed component with header container

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Feed from "../components/Feed";
 import Sidebar from "../components/Sidebar";
 
 export default function Home() {
@@ -11,7 +12,7 @@ export default function Home() {
 
       <main className="bg-black min-h-screen flex max-w-[1500px] mx-auto text-white">
         <Sidebar />
-        {/* Feed */}
+        <Feed />
         {/* Widgets */}
 
         {/* Modal */}
