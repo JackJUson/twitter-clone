f6ca2e151119067ac9b7b8a3dfe14f9a8d1a41a3
Jackson <z5397791@ad.unsw.edu.au>
2023-01-06 17:04:43 +1100

    Add text area container  with setInput hook to capture text

--- components/Input.js
+++ components/Input.js
@@ -6,7 +6,7 @@ function Input() {
   return (
     <div
       className={`border-b border-gray-700 p-3 flex space-x-3
-    overflow-y-scroll`}
+    overflow-y-scroll scrollbar-hide`}
     >
       <img
         src="https://avatars.githubusercontent.com/u/108652931?v=4"
@@ -17,9 +17,12 @@ function Input() {
         <div className={``}>
           <textarea
             value={input}
+            onChange={(event) => setInput(event.target.value)}
             rows="2"
-            className="bg-transparent outline-none"
-          ></textarea>
+            placeholder="What's happening?"
+            className="bg-transparent outline-none text-[#d9d9d9] text-lg
+             placeholder-gray-500 tracking-wide w-full min-h-[50px] scrollbar-hide"
+          />
         </div>
         <div>Hello</div>
       </div>
