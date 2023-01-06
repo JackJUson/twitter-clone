a5bbb2ae9c2adf21f49a23069a5a9c6928133b1d
Jackson <z5397791@ad.unsw.edu.au>
2023-01-06 17:03:35 +1100

    Install tailwind scrollbar hide plugin

--- components/Input.js
+++ components/Input.js
@@ -1,12 +1,29 @@
+import { useState } from "react";
+
 function Input() {
+  const [input, setInput] = useState();
+
   return (
-    <div>
+    <div
+      className={`border-b border-gray-700 p-3 flex space-x-3
+    overflow-y-scroll`}
+    >
       <img
-          src="https://avatars.githubusercontent.com/u/108652931?v=4"
-          alt=""
-          className="h-11 w-11 rounded-full cursor-pointer"
-        />
+        src="https://avatars.githubusercontent.com/u/108652931?v=4"
+        alt=""
+        className="h-11 w-11 rounded-full cursor-pointer"
+      />
+      <div className="w-full divide-y divide-gray-700">
+        <div className={``}>
+          <textarea
+            value={input}
+            rows="2"
+            className="bg-transparent outline-none"
+          ></textarea>
+        </div>
+        <div>Hello</div>
+      </div>
     </div>
-  )
+  );
 }
-export default Input
+export default Input;
