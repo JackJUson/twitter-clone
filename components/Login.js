57c501bf0967d8967de8958e8d5a832921ff06de
Jackson <z5397791@ad.unsw.edu.au>
2023-01-08 13:10:14 +1100

    Add login component with home icon

---
+++ components/Login.js
@@ -0,0 +1,15 @@
+import Image from "next/image"
+
+function Login() {
+  return (
+    <div className="flex flex-col items-center space-y-20 pt-48">
+      <img
+        src="https://icon-library.com/images/twitter-icon-svg/twitter-icon-svg-28.jpg"
+        width={150}
+        height={150}
+        className="object-contain"
+      />
+    </div>
+  )
+}
+export default Login
