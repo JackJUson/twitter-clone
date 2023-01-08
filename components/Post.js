b39f9e88f15e50c2ec89cc182738bd9e525bd348
Jackson <z5397791@ad.unsw.edu.au>
2023-01-08 17:34:07 +1100

    Add user name and tag div to post component

--- components/Post.js
+++ components/Post.js
@@ -1,12 +1,39 @@
-function Post({ id, post, postPage }) {
+function Post({ id, post, postData }) {
   return (
     <div className="p-3 flex cursor-pointer border-b border-gray-700">
-      {!postPage && (
-        <div>
-          <img src={post?.userImg} alt="" />
-          <p>{post?.text}</p>
-        </div>
+      {!postData && (
+        <img
+          src={post?.userImg}
+          alt=""
+          className="h-11 w-11 rounded-full mr-4"
+        />
       )}
+      <div className="flex flex-col space-y-2 w-full">
+        <div className={`flex ${!postData && "justify-between"}`}>
+          {postData && (
+            <img
+              src={post?.userImg}
+              alt="Profile Pic"
+              className="h-11 w-11 rounded-full mr-4"
+            />
+          )}
+          <div className="text-[#62767d]">
+            <div className="inline-block group">
+              <h4
+                className={`font-bold text-[15px] sm:text-base text-[#d9d9d9] 
+                group-hover:underline ${!postData && "inline-block"}`}
+              >
+                {post?.username}
+              </h4>
+              <span
+                className={`text-sm sm:text-[15px] ${!postData && "ml-1.5"}`}
+              >
+                @{post?.tag}
+              </span>
+            </div>{" "}·{" "}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
