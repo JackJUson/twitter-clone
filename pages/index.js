038a587a327e7caf868dd91f816c304ea675c2c8
Jackson <z5397791@ad.unsw.edu.au>
2023-01-08 15:19:19 +1100

    Add useSession from next auth to retrive session data

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,14 @@ import Head from "next/head";
 import Feed from "../components/Feed";
 import Sidebar from "../components/Sidebar";
 import { getProviders, getSession, useSession } from "next-auth/react";
+import Login from "../components/Login";
+import axios from "axios";
 
-export default function Home() {
+export default function Home({ trendingResults, followResults, providers }) {
+  const { data: session } = useSession();
+
+  if (!session) return <Login providers={providers} />;
+
   return (
     <div className="">
       <Head>
@@ -23,13 +29,15 @@ export default function Home() {
 }
 
 export async function getServerSideProps(context) {
-  const trendingResults = axios
-    .get(`https://jsonplaceholder.typicode.com/users`)
-    .then((response) => response.json());
-  const followResults = axios
-    .get(`https://jsonplaceholder.typicode.com/users`)
-    .then((response) => response.json());
+  const trendingResults = await fetch("https://jsonplaceholder.typicode.com/users").then(
+    (res) => res.json()
+  );
+  const followResults = await fetch("https://jsonplaceholder.typicode.com/users").then(
+    (res) => res.json()
+  );
   const providers = await getProviders();
+
+  // Prevents login page to show up by react flickering
   const session = await getSession(context);
 
   return {
