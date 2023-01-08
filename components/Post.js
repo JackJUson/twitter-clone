d61e6334954cbab1ac0e14a4f657434dad672def
Jackson <z5397791@ad.unsw.edu.au>
2023-01-08 17:07:46 +1100

    Add post user image and text if postPage data is available

--- components/Post.js
+++ components/Post.js
@@ -1,6 +1,13 @@
-function Post() {
+function Post({ id, post, postPage }) {
   return (
-    <div>Post</div>
-  )
+    <div className="p-3 flex cursor-pointer border-b border-gray-700">
+      {!postPage && (
+        <div>
+          <img src={post?.userImg} alt="" />
+          <p>{post?.text}</p>
+        </div>
+      )}
+    </div>
+  );
 }
-export default Post
+export default Post;
