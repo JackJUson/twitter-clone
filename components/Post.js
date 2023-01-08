14ea3d2372d0995d96a9fee8e3481ed9fac3f474
Jackson <z5397791@ad.unsw.edu.au>
2023-01-08 19:43:34 +1100

    Add comment and like button to post container

--- components/Post.js
+++ components/Post.js
@@ -1,15 +1,67 @@
-import {
-  ArrowsRightLeftIcon,
-  ChatBubbleLeftIcon,
-  EllipsisHorizontalIcon,
-  ShareIcon,
-  TrashIcon,
-} from "@heroicons/react/24/outline";
+import { ArrowsRightLeftIcon, ChatBubbleLeftIcon, EllipsisHorizontalIcon, ShareIcon, TrashIcon } from "@heroicons/react/24/outline";
 import { HeartIcon, ChartBarIcon } from "@heroicons/react/24/solid";
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import { modalState, postIdState } from "../atoms/modalAtom";
+import { db } from "../firebase";
+
+import {
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  orderBy,
+  query,
+  setDoc,
+} from "@firebase/firestore";
 
 function Post({ id, post, postData }) {
   const { data: session } = useSession();
+  const [isOpen, setIsOpen] = useRecoilState(modalState);
+  const [postId, setPostId] = useRecoilState(postIdState);
+  const [comments, setComments] = useState([]);
+  const [likes, setLikes] = useState([]);
+  const [liked, setLiked] = useState(false);
+
+  useEffect(
+    () =>
+      onSnapshot(
+        query(
+          collection(db, "posts", id, "comments"),
+          orderBy("timestamp", "desc")
+        ),
+        (snapshot) => setComments(snapshot.docs)
+      ),
+    [db, id]
+  );
+
+  useEffect(
+    () =>
+      onSnapshot(collection(db, "posts", id, "likes"), (snapshot) =>
+        setLikes(snapshot.docs)
+      ),
+    [db, id]
+  );
+
+  useEffect(
+    () =>
+      setLiked(
+        likes.findIndex((like) => like.id === session?.user?.uid) !== -1
+      ),
+    [likes]
+  );
+
+  const likePost = async () => {
+    if (liked) {
+      await deleteDoc(doc(db, "posts", id, "likes", session.user.uid));
+    } else {
+      await setDoc(doc(db, "posts", id, "likes", session.user.uid), {
+        username: session.user.name,
+      });
+    }
+  };
 
   return (
     <div className="p-3 flex cursor-pointer border-b border-gray-700">
@@ -51,7 +103,8 @@ function Post({ id, post, postData }) {
             )}
           </div>
           <div className="icon group flex-shrink-0 ml-auto">
-            <EllipsisHorizontalIcon className="h-5 text-[#6e767d] group-hover:text-[#1d9bf0]" />
+            <EllipsisHorizontalIcon 
+            className="h-5 text-[#6e767d] group-hover:text-[#1d9bf0]" />
           </div>
         </div>
 
@@ -70,9 +123,53 @@ function Post({ id, post, postData }) {
             postData && "mx-auto"
           }`}
         >
+          <div
+            className="flex items-center space-x-1 group"
+            onClick={(event) => {
+              event.stopPropagation();
+              setPostId(id);
+              setIsOpen(true);
+            }}
+          >
+            <div className="icon group-hover:bg-[#1d9bf0] group-hover:bg-opacity-10">
+              <ChatBubbleLeftIcon className="h-5 group-hover:text-[#1d9bf0]" />
+            </div>
+            {comments.length > 0 && (
+              <span className="group-hover:text-[#1d9bf0] text-sm">
+                {comments.length}
+              </span>
+            )}
+          </div>
+
+          <div
+            className="flex items-center space-x-1 group"
+            onClick={(e) => {
+              e.stopPropagation();
+              likePost();
+            }}
+          >
+            <div className="icon group-hover:bg-pink-600/10">
+              {liked ? (
+                <HeartIcon className="h-5 text-pink-600" />
+              ) : (
+                <HeartIcon className="h-5 group-hover:text-pink-600" />
+              )}
+            </div>
+            {likes.length > 0 && (
+              <span
+                className={`group-hover:text-pink-600 text-sm ${
+                  liked && "text-pink-600"
+                }`}
+              >
+                {likes.length}
+              </span>
+            )}
+          </div>
+
           <div className="icon group">
             <ShareIcon className="h-5 group-hover:text-[#1d9bf0]" />
           </div>
+
           <div className="icon group">
             <ChartBarIcon className="h-5 group-hover:text-[#1d9bf0]" />
           </div>
