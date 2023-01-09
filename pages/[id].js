b4166d81b9eae327814605e4afd7b5105a1640a7
Jackson <z5397791@ad.unsw.edu.au>
2023-01-10 10:28:28 +1100

    Add post component into main div of post page

--- pages/[id].js
+++ pages/[id].js
@@ -46,6 +46,21 @@ function PostPage() {
 
       <main className="bg-black min-h-screen flex max-w-[1500px] mx-auto text-white">
         <Sidebar />
+        <div className="flex-grow border-l border-r border-gray-700 max-w-2xl 
+        sm:ml-[73px] xl:ml-[370px]">
+          <div className="flex items-center px-1.5 py-2 border-b border-gray-700 
+          text-[#d9d9d9] font-semibold text-xl gap-x-4 sticky top-0 z-50 bg-black">
+            <div
+              className="hover__animation w-9 h-9 flex items-center justify-center xl:px-0"
+              onClick={() => router.push("/")}
+            >
+              <ArrowLeftIcon className="h-5 text-white" />
+            </div>
+            Tweet
+          </div>
+
+          <Post id={id} post={post} postPage />
+        </div>
         {/* Widgets */}
 
         {isOpen && <Modal />}
