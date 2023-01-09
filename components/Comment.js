0bae261f44f5bdf233f0f70cc4866e8adaaaa600
Jackson <z5397791@ad.unsw.edu.au>
2023-01-10 10:45:59 +1100

    Add comment user image, username and tag to comment component

--- components/Comment.js
+++ components/Comment.js
@@ -1,4 +1,26 @@
 function Comment({ id, comment }) {
-  return <div>Comment</div>;
+  return (
+    <div className="p-3 flex cursor-pointer border-b border-gray-700">
+      <img
+        src={comment?.userImg}
+        className="h-11 w-11 rounded-full mr-4"
+        alt=""
+      />
+      <div className="flex flex-col space-y-2 w-full">
+        <div className="flex justify-between">
+          <div className="text-[#6e767d]">
+            <div className="inline-block group">
+              <h4 className="font-bold text-[#d9d9d9] text-[15px] sm:text-base inline-block group-hover:underline">
+                {comment?.username}
+              </h4>
+              <span className="ml-1.5 text-sm sm:text-[15px]">
+                @{comment?.tag}{" "}
+              </span>
+            </div>{" "}·{" "}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Comment;
