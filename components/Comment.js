c48a3513065a0949ef9e2dd70d135e8c7e35c107
Jackson <z5397791@ad.unsw.edu.au>
2023-01-10 10:53:29 +1100

    Add comment text and option icon in comment container

--- components/Comment.js
+++ components/Comment.js
@@ -1,3 +1,6 @@
+import { EllipsisHorizontalCircleIcon, EllipsisHorizontalIcon } from "@heroicons/react/24/outline";
+import Moment from "react-moment";
+
 function Comment({ id, comment }) {
   return (
     <div className="p-3 flex cursor-pointer border-b border-gray-700">
@@ -10,13 +13,29 @@ function Comment({ id, comment }) {
         <div className="flex justify-between">
           <div className="text-[#6e767d]">
             <div className="inline-block group">
-              <h4 className="font-bold text-[#d9d9d9] text-[15px] sm:text-base inline-block group-hover:underline">
+              <h4
+                className="font-bold text-[#d9d9d9] text-[15px] sm:text-base 
+              inline-block group-hover:underline"
+              >
                 {comment?.username}
               </h4>
               <span className="ml-1.5 text-sm sm:text-[15px]">
                 @{comment?.tag}{" "}
               </span>
-            </div>{" "}·{" "}
+            </div>{" "}
+            ·{" "}
+            <span className="hover:underline text-sm sm:text-[15px]">
+              <Moment fromNow>{comment?.timestamp?.toDate()}</Moment>
+            </span>
+            <p
+              className="text-[#d9d9d9] mt-0.5 max-w-lg overflow-scroll 
+            scrollbar-hide text-[15px] sm:text-base"
+            >
+              {comment?.comment}
+            </p>
+          </div>
+          <div className="icon group flex-shrink-0">
+            <EllipsisHorizontalIcon className="h-5 text-[#6e767d] group-hover:text-[#1d9bf0]" />
           </div>
         </div>
       </div>
