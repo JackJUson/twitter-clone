075bef6f3a899e55b64b63afd0eafa1ea3f7e270
Jackson <z5397791@ad.unsw.edu.au>
2023-01-10 10:21:13 +1100

    Add useffect hook and serversideprops from home page into post page

--- pages/[id].js
+++ pages/[id].js
@@ -1,20 +1,46 @@
-import { useSession } from "next-auth/react";
-import Head from "next/head"
+import { getProviders, getSession, useSession } from "next-auth/react";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import { modalState } from "../atoms/modalAtom";
-import Modal from "../components/Modal"
-import Sidebar from "../components/Sidebar"
+import Login from "../components/Login";
+import Modal from "../components/Modal";
+import Sidebar from "../components/Sidebar";
+import { db } from "../firebase";
+import {
+  collection,
+  doc,
+  onSnapshot,
+  orderBy,
+  query,
+} from "@firebase/firestore";
+import Post from "../components/Post";
+import { ArrowLeftIcon } from "@heroicons/react/24/outline";
 
 function PostPage() {
   const { data: session } = useSession();
   const [isOpen, setIsOpen] = useRecoilState(modalState);
+  const [post, setPost] = useState();
   const router = useRouter();
   const { id } = router.query;
+
+  useEffect(
+    () =>
+      onSnapshot(doc(db, "posts", id), (snapshot) => {
+        setPost(snapshot.data());
+      }),
+    [db]
+  );
+
+  if (!session) return <Login providers={providers} />;
 
   return (
     <div className="">
       <Head>
-        <title>Twitter</title>
+        <title>
+          {post?.username} on Twitter: "{post?.text}"
+        </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -25,6 +51,27 @@ function PostPage() {
         {isOpen && <Modal />}
       </main>
     </div>
-  )
+  );
 }
-export default PostPage
+
+export default PostPage;
+
+export async function getServerSideProps(context) {
+  const trendingResults = await fetch(
+    "https://jsonplaceholder.typicode.com/users"
+  ).then((res) => res.json());
+  const followResults = await fetch(
+    "https://jsonplaceholder.typicode.com/users"
+  ).then((res) => res.json());
+  const providers = await getProviders();
+  const session = await getSession(context);
+
+  return {
+    props: {
+      trendingResults,
+      followResults,
+      providers,
+      session,
+    },
+  };
+}
