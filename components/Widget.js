3cd7f77bf29b165e4454e92018ef6b476811ccb2
Jackson <z5397791@ad.unsw.edu.au>
2023-01-10 12:21:14 +1100

    Add follow result container below trending results

--- components/Widget.js
+++ components/Widget.js
@@ -18,15 +18,27 @@ function Widget({ trendingResults, followResults }) {
         </div>
       </div>
 
-      <div className="text-[#d9d9d9] space-y-3 bg-[#15181c] pt-2 rounded-xl 
-      w-11/12 xl:w-9/12">
+      <div
+        className="text-[#d9d9d9] space-y-3 bg-[#15181c] pt-2 rounded-xl 
+      w-11/12 xl:w-9/12"
+      >
         <h4 className="font-bold text-xl px-4">What's happening?</h4>
         {trendingResults.map((result, index) => (
-          <Trending key={index} result={result}/>
+          <Trending key={index} result={result} />
         ))}
-        <button className="hover:bg-white hover:bg-opacity-[0.03] px-4 py-3 
-        cursor-pointer transition duration-200 ease-out flex items-center 
-        justify-between w-full text-[#1d9bf0] font-light">Show more</button>
+        <button className="widget_button">Show more</button>
+      </div>
+
+      <div
+        className="text-[#d9d9d9] space-y-3 bg-[#15181c] pt-2 rounded-xl 
+      w-11/12 xl:w-9/12"
+      >
+        <h4 className="font-bold text-xl px-4">Who to follow</h4>
+        {followResults.map((result, index) => (
+          <div>
+          </div>
+        ))}
+        <button className="widget_button">Show more</button>
       </div>
     </div>
   );
