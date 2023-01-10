963b68d80ab93ee6cbb153407ffdab95541b9049
Jackson <z5397791@ad.unsw.edu.au>
2023-01-10 12:26:40 +1100

    Add follow result image, username and tag to widget component

--- components/Widget.js
+++ components/Widget.js
@@ -35,7 +35,27 @@ function Widget({ trendingResults, followResults }) {
       >
         <h4 className="font-bold text-xl px-4">Who to follow</h4>
         {followResults.map((result, index) => (
-          <div>
+          <div
+            className="hover:bg-white hover:bg-opacity-[0.03] px-4 py-2 
+            cursor-pointer transition duration-200 ease-out flex items-center"
+            key={index}
+          >
+            <img
+              src={result.userImg}
+              width={50}
+              height={50}
+              className="rounded-full object-cover"
+            />
+            <div className="ml-4 leading-5 group">
+              <h4 className="font-bold group-hover:underline">
+                {result.username}
+              </h4>
+              <h5 className="text-gray-500 text-[15px]">{result.tag}</h5>
+            </div>
+            <button className="ml-auto bg-white text-black rounded-full 
+            font-bold text-sm py-1.5 px-3.5">
+              Follow
+            </button>
           </div>
         ))}
         <button className="widget_button">Show more</button>
