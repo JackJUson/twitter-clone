f42b54b8c9c72892d9d10f6643cfa84918c90b50
Jackson <z5397791@ad.unsw.edu.au>
2023-01-10 12:12:13 +1100

    Add result heading, description and tags on trending component

--- components/Trending.js
+++ components/Trending.js
@@ -1,6 +1,22 @@
-function Trending() {
+function Trending({ result }) {
   return (
-    <div>Trending</div>
-  )
+    <div className="hover:bg-white hover:bg-opacity-[0.03] px-4 py-2 cursor-pointer transition duration-200 ease-out flex items-center justify-between">
+      <div className="space-y-0.5">
+        <p className="text-[#6e767d] text-xs font-medium">{result.heading}</p>
+        <h6 className="font-bold max-w-[250px] text-sm">
+          {result.description}
+        </h6>
+        <p className="text-[#6e767d] text-xs font-medium max-w-[250px]">
+          Trending with{" "}
+          {result.tags.map((tag, index) => (
+            <span className="tag" key={index}>
+              {tag}
+            </span>
+          ))}
+        </p>
+      </div>
+    </div>
+  );
 }
-export default Trending
+
+export default Trending;
