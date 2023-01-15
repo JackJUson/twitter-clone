08cf1cb5cdb5beec9be9fff2d8e92baa4e715719
Jackson <z5397791@ad.unsw.edu.au>
2023-01-16 09:40:24 +1100

    Add auth secret for JWEDecrytion failure

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
+  secret: process.env.AUTH_SECRET,
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
@@ -21,7 +22,6 @@ export const authOptions = {
       return session;
     },
   },
-  secret: process.env.JWT_SECRET,
 };
 
 export default NextAuth(authOptions);
